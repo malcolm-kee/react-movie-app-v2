f4727ed27b89c21a7e53b5d6c8c5cef6c884ce9e
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-02-09 00:28:34 +0800

    useState hook

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Movie } from './components/movie';
 import { TitleBar } from './components/title-bar';
+import { Button } from './components/button';
 
 function App() {
+  const [moviesShown, setShowMovies] = React.useState(false);
+
   return (
     <div>
       <TitleBar>
         <h1>React Movie App</h1>
       </TitleBar>
-      <Movie name="Aquaman" releaseDate="2018-12-07" />
-      <Movie name="Bumblebee" releaseDate="2018-12-15" />
-      <Movie
-        name="Fantastic Beasts: The Crimes of Grindelwald"
-        releaseDate="2018-11-14"
-      />
+      <div className="button-container">
+        <Button onClick={() => setShowMovies(prevShown => !prevShown)}>
+          Show Movies
+        </Button>
+      </div>
+      {moviesShown && (
+        <>
+          <Movie name="Aquaman" releaseDate="2018-12-07" />
+          <Movie name="Bumblebee" releaseDate="2018-12-15" />
+          <Movie
+            name="Fantastic Beasts: The Crimes of Grindelwald"
+            releaseDate="2018-11-14"
+          />
+        </>
+      )}
     </div>
   );
 }
