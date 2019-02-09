55cfe8ae71ccfa452f0d7fd7e6f0d6ba733089d2
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-02-09 14:18:19 +0800

    add dynamic import

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
+const getAxios = () => import('axios');
 
 const MOVIE_ENDPOINT = 'https://react-intro-movies.herokuapp.com/movies';
 
-export const loadMovies = () => axios(MOVIE_ENDPOINT).then(res => res.data);
+export const loadMovies = () =>
+  getAxios()
+    .then(axios => axios.default(MOVIE_ENDPOINT))
+    .then(res => res.data);
 
 export const createMovie = movie =>
-  axios.post(MOVIE_ENDPOINT, movie).then(res => res.data);
+  getAxios()
+    .then(axios => axios.post(MOVIE_ENDPOINT, movie))
+    .then(res => res.data);
 
 export const saveMovie = movie =>
-  axios.put(`${MOVIE_ENDPOINT}/${movie.id}`, movie).then(res => res.data);
+  getAxios()
+    .then(axios => axios.put(`${MOVIE_ENDPOINT}/${movie.id}`, movie))
+    .then(res => res.data);
